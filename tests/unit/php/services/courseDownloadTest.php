a50f9de1dcfc41d3779f70dac467b59a5b1f0559
Haastrup Elijah <haastrupea@gmail.com>
2019-11-12 09:26:50 +0100

    course download test

---
+++ tests/unit/php/services/courseDownloadTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Viralvibes\Test;
+require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+require dirname(__FILE__,5).DIRECTORY_SEPARATOR. 'models'. DIRECTORY_SEPARATOR. 'php'.DIRECTORY_SEPARATOR. 'services'.DIRECTORY_SEPARATOR.'courseDownload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class courseDownloadTest extends TestCase{
+    public function download()
+    {
+        # code...
+    }
+}
