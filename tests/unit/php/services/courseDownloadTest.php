92a2529e802c2cad3c4f7d7983381db418efb01a
Haastrup Elijah <haastrupea@gmail.com>
2019-11-14 10:39:06 +0100

    add report link table add test for user already report add test for link report

--- tests/unit/php/services/courseDownloadTest.php
+++ tests/unit/php/services/courseDownloadTest.php
@@ -29,23 +29,16 @@ class courseDownloadTest extends TestCase{
 
     public function createDbTable(){
         $db=$this->dbConnection;
-        $query1="CREATE TABLE `courses` (
-            `course_id` int,
-            `institution` varchar(100) NOT NULL,
-            `course_code` varchar(10) NOT NULL,
-            `course_title` varchar(100) NOT NULL,
-            `department` varchar(500) NOT NULL,
-            `session` varchar(10) DEFAULT NULL,
-            `semester` varchar(10) DEFAULT NULL,
-            `view_count` int(11) DEFAULT '0',
-            `published` TINYINT(1) NOT NULL DEFAULT '1',
-            `when_added` TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `last_update` TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `description` varchar(255) NOT NULL,
-            `course_type` varchar(50) NOT NULL,
-            `course_unit` int NOT NULL,
-            `name_is_acronym` TINYINT(1) NOT NULL DEFAULT '0',
-            PRIMARY KEY (`course_id`)
+        $query1="CREATE TABLE `report_link` (
+            `report_id` int,
+            `link_id` int(11) NOT NULL,
+            `user_id` int(11) NOT NULL,
+            `report_date` text NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `resolved` tinyint(1) NOT NULL DEFAULT '0',
+            `date_resolved` text NULL DEFAULT '0000-00-00 00:00:00',
+            `resolved_by` int(11) DEFAULT NULL,
+            `reason` varchar(255) NOT NULL,
+            PRIMARY KEY (`report_id`)
           );";
           $db->queryDb($query1);
 
@@ -63,16 +56,8 @@ class courseDownloadTest extends TestCase{
 
     public function buildDataSet(){
         $db=$this->dbConnection;
-        $query1="INSERT INTO `courses` (`course_id`, `institution`, `course_code`, `course_title`, `department`, `session`, `semester`, `view_count`, `published`, `when_added`, `last_update`, `description`, `course_type`, `course_unit`, `name_is_acronym`) VALUES
-        (1, 'Obafemi Awolowo University', 'SEM001', 'MAN AND HIS ENVIRONMENT', 'animal science', '2018/2019', '2', 20, 1, '2019-11-01 10:47:17', '2019-11-01 10:47:17', 'no description for now', 'special elective', 2, 0),
-        (2, 'obafemi Awolowo University', 'SEM002', 'man and people', 'Estate mangement', '2018/2019', '1', 0, 1, '2019-11-06 00:23:16', '2019-11-06 00:23:16', 'compostry for all student that wants to graduate', 'restricted elective', 4, 0),
-        (3, 'obafemi Awolowo University', 'seroo1', 'introduction to English', 'all department', NULL, NULL, 0, 1, '2019-11-06 00:23:16', '2019-11-06 00:23:16', '', 'special elective', 0, 0),
-        (4, 'obafemi Awolowo University', 'SEM004', 'asking question', 'a.b.c.d', '2018/2019', '1', 0, 1, '2019-11-06 00:28:41', '2019-11-06 00:28:41', 'wonder but easy to pass', 'restricted elective', 4, 0),
-        (5, 'obafemi Awolowo University', 'ans301', 'introduction to ruminant', 'animal science, agricultural economics', '2018/2019', '1', 0, 1, '2019-11-06 00:28:41', '2019-11-06 00:28:41', 'for all department except fncs', 'core', 3, 0),
-        (6, 'obafemi Awolowo University', 'ans302', 'introduction to non-ruminant', 'animal science, agricultural economics', '2018/2019', '1', 0, 1, '2019-11-06 00:28:41', '2019-11-06 00:28:41', 'for all department except fncs', 'core', 3, 0),
-        (7, 'obafemi Awolowo University', 'cpp301', 'introduction to non-ruminant', 'animal science, agricultural economics', '2018/2019', '1', 0, 1, '2019-11-06 00:28:41', '2019-11-06 00:28:41', 'for all department except fncs', 'core', 3, 0),
-        (8, 'obafemi Awolowo University', 'cpp302', 'introduction to non-ruminant', 'animal science, agricultural economics', '2018/2019', '1', 0, 1, '2019-11-06 00:28:41', '2019-11-06 00:28:41', 'for all department except fncs', 'core', 3, 0),
-        (9, 'obafemi Awolowo University', 'axd301', 'introduction to non-ruminant', 'animal science, agricultural economics', '2018/2019', '1', 0, 1, '2019-11-06 00:28:41', '2019-11-06 00:28:41', 'for all department except fncs', 'core', 3, 0);";
+        $query1="INSERT INTO `report_link` (`link_id`, `user_id`,`reason`) VALUES
+        (5, 1, 'link is broken'),(5, 2,'file has been deleted');";
          $db->queryDb($query1);
 
         $query="INSERT INTO `dl_Course_link` (`dl_id`, `dl_link`, `course_id`, `external_link`, `dl_count`) VALUES 
@@ -111,5 +96,21 @@ class courseDownloadTest extends TestCase{
         $link->updateDownloadCount();
         $out=$link->get_dl_count();
         $this->assertEquals(6,$out,"Expect download count of 6");
+    }
+    
+
+    public function test_user_already_report_link()
+    {
+        $link= new downloadLink(5,$this->dbConnection);
+        $result=$link->areadyReportlink(1);
+        $this->assertTrue($result," The user was expected to have reported  this link as broken already");
+    }
+
+    public function test_user_report_a_link()
+    {
+        $link= new downloadLink(5,$this->dbConnection);
+        $link->reportLink(3,"just testing report link feature");
+        $result=$link->areadyReportlink(3);
+        $this->assertTrue($result," expect to have added '1' more report for this link");
     }
 }
