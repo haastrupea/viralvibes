d8063eda1fd8ad26d5115939c10b15560a0d8e65
Haastrup Elijah <haastrupea@gmail.com>
2019-11-22 23:48:15 +0100

    adding test for database

--- tests/unit/php/services/databaseTest.php
+++ tests/unit/php/services/databaseTest.php
@@ -1,1 +1,14 @@
-require ""
+<?php
+namespace Viralvibes\Test;
+require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+use PHPUnit\Framework\TestCase;
+use Viralvibes\database;
+
+class databaseTest extends TestCase{
+    static protected $dbcon;
+    static public function setUpBeforeClass(): void
+    {
+        self::$dbcon=database::getInstance('sqlite',':memory:');
+    }
+
+}
