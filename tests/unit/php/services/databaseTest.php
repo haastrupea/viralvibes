50a63a6d8ab6f0daeeaba6fa44c8b161d1bca4ac
Haastrup Elijah <haastrupea@gmail.com>
2019-11-23 00:12:56 +0100

    test connection to databse

--- tests/unit/php/services/databaseTest.php
+++ tests/unit/php/services/databaseTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Viralvibes\Test;
 require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'services'.DIRECTORY_SEPARATOR.'database.php';
+
 use PHPUnit\Framework\TestCase;
 use Viralvibes\database;
 
@@ -9,6 +11,13 @@ class databaseTest extends TestCase{
     static public function setUpBeforeClass(): void
     {
         self::$dbcon=database::getInstance('sqlite',':memory:');
+    }
+
+    public function test_database_connect_successfully()
+    {
+        $out=self::$dbcon->getConnection();
+
+        $this->assertInstanceOf(\PDO::class,$out,"failed to connnect to database");
     }
 
 }
