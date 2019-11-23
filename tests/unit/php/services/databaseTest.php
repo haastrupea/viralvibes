a3df2804e5b491fee44b6f111d3caad165332acf
Haastrup Elijah <haastrupea@gmail.com>
2019-11-23 17:38:57 +0100

    clean up require statement after adding clasmap

--- tests/unit/php/services/databaseTest.php
+++ tests/unit/php/services/databaseTest.php
@@ -1,8 +1,6 @@
 <?php
 namespace Viralvibes\Test;
 require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'services'.DIRECTORY_SEPARATOR.'database.php';
-require 'tests'.DIRECTORY_SEPARATOR.'unit'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'services'.DIRECTORY_SEPARATOR.'databasetrait.php';
 
 use PHPUnit\Framework\TestCase;
 use Viralvibes\database;
