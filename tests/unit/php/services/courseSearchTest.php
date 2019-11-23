6df87e2a1cdafd8dac2bbad1f2a2806bad4fc4e4
Haastrup Elijah <haastrupea@gmail.com>
2019-11-23 10:01:41 +0100

    update coure search test

---
+++ tests/unit/php/services/courseSearchTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Viralvibes\Test;
+require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+use PHPUnit\Framework\TestCase;
+
+class courseSearchTest extends TestCase{
+    
+}
