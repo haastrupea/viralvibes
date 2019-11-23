059c55357b886d766ebd58722f48b2ba68f74387
Haastrup Elijah <haastrupea@gmail.com>
2019-11-23 10:00:30 +0100

    import database trait

--- tests/unit/php/services/databaseTest.php
+++ tests/unit/php/services/databaseTest.php
@@ -2,12 +2,15 @@
 namespace Viralvibes\Test;
 require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 require dirname(__FILE__,5).DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'services'.DIRECTORY_SEPARATOR.'database.php';
+require 'tests'.DIRECTORY_SEPARATOR.'unit'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'services'.DIRECTORY_SEPARATOR.'databasetrait.php';
 
 use PHPUnit\Framework\TestCase;
 use Viralvibes\database;
+use Viralvibes\Test\databasetrait;
 
 class databaseTest extends TestCase{
     static protected $dbcon;
+    use databasetrait;
     static public function setUpBeforeClass(): void
     {
         self::$dbcon=database::getInstance('sqlite',':memory:');
@@ -22,40 +25,6 @@ class databaseTest extends TestCase{
         $qry="DROP TABLE IF EXISTS courses";
         self::$dbcon->getConnection()->exec($qry);
         self::$dbcon=null;
-    }
-
-    static public function createDbTable(){
-        $db=self::$dbcon->getConnection();
-        $query="CREATE TABLE IF NOT EXISTS `courses` (
-            `id` int,
-            `institution` varchar(100) NOT NULL,
-            `code` varchar(10) NOT NULL,
-            `title` varchar(100) NOT NULL,
-            `department` varchar(500) NOT NULL,
-            `session` varchar(10) DEFAULT NULL,
-            `semester` varchar(10) DEFAULT NULL,
-            `view_count` int(11) DEFAULT '0',
-            `published` TINYINT(1) DEFAULT '1',
-            `when_added` TEXT DEFAULT CURRENT_TIMESTAMP,
-            `last_update` TEXT DEFAULT CURRENT_TIMESTAMP,
-            `description` varchar(255) NOT NULL,
-            `type` varchar(50) NOT NULL,
-            `unit` int NOT NULL,
-            `school_name_is_acronym` TINYINT(1) DEFAULT '0',
-            PRIMARY KEY (`id`)
-          );";
-          $db->exec($query);
-    }
-    static public function buildDataSet(){
-        $db=self::$dbcon->getConnection();
-        $query="INSERT INTO `courses` (id,`institution`, `code`, `title`, `department`, `session`, `semester`,`description`, `type`, `unit`, `school_name_is_acronym`) VALUES
-        (1,'Obafemi Awolowo University', 'SEM001', 'MAN AND HIS ENVIRONMENT', 'animal science', '2018/2019', '2', 'no description for now', 'special elective', 2, 0),
-        (2,'obafemi Awolowo University', 'SEM002', 'man and people', 'Estate mangement', '2018/2019', '1','compostry for all student that wants to graduate', 'restricted elective', 4, 0),
-        (3,'obafemi Awolowo University', 'seroo1', 'introduction to English', 'all department', NULL, NULL,'', 'special elective', 0, 0),
-        (4,'obafemi Awolowo University', 'SEM004', 'asking question', 'a.b.c.d', '2018/2019', '1', 'wonder but easy to pass', 'restricted elective', 4, 0),
-        (5,'obafemi Awolowo University', 'ans301', 'introduction to ruminant', 'animal science, agricultural economics', '2018/2019', '1', 'for all department except fncs', 'core', 3, 0),
-        (6,'obafemi Awolowo University', 'ans302', 'introduction to non-ruminant', 'animal science, agricultural economics', '2018/2019', '1', 'for all department except fncs', 'core', 3, 0);";
-          $db->exec($query);
     }
 
     public function test_database_connect_successfully()
