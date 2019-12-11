b496df7b644b69e12b6516f3aaab0728a6c2b36a
Haastrup Elijah <haastrupea@gmail.com>
2019-12-11 11:44:53 +0100

    mvc impliment

--- controller/php/controllersLoader.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @todo load all the controller with spl autoloader
- */
-include_once 'controller/php/defaultController.php';
-include_once 'controller/php/downloadController.php';
-include_once 'controller/php/housingController.php';
-include_once 'controller/php/newsController.php';
--- models/php/modelsLoader.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * @todo load all the models with spl autoloader
- */
-include_once 'models/php/homePage.php';
