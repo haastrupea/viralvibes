4948c0735b4084fbf871b1e1d261849e12ec7915
Haastrup Elijah <haastrupea@gmail.com>
2019-12-13 01:20:39 +0100

    added site wide config

--- config/config.php
+++ config/config.php
@@ -1,4 +1,7 @@
 <?
-$app=['config'=>"{
-    database:'[]'
-}"];
+$setup['db_type']='mysql';
+$setup['db_name']='dbname';
+$setup['db_password']='password';
+$setup['db_user']='username';
+$setup['doc_upload_dir']="/assets/upload/document/";
+$setup['route_dir']='/route/route.';
